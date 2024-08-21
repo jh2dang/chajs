79b7dfa0067e834e6d6f46d20dbdfa8071fa9d82
jh2dang <bluemin_@naver.com>
2024-08-22 08:35:37 +0900

    Feat : test釉뚮옖移섏뿉 stash

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ export default function Header() {
   return (
     <div className="h-[56px] flex justify-between items-center p-4">
       <Image src={minilogo} alt="..." />
-      <div className="flex">
+      <div className="flex items-center">
         <p className="text-lg font-medium">부산</p>
-        <Image src={arrow} alt="..." />
+        <Image src={arrow} alt="..." className="w-[18px] h-[18px]" />
       </div>
       <Image src={bell} alt="..." />
     </div>
